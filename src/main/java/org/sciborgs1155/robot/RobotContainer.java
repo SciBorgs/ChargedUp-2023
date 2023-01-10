ad47b948dcddbd0895fbfdd973f31dbcc2e62ca6
Tabeeb <chowdhurt4@bxscience.edu>
2023-01-10 17:54:18 -0500

    Ported Swerve to 2023 WPILIB Odometry

--- src/main/java/org/sciborgs1155/robot/RobotContainer.java
+++ src/main/java/org/sciborgs1155/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import io.github.oblarg.oblog.Logger;
-import org.sciborgs1155.robot.Constants.OperatorConstants;
+import org.sciborgs1155.robot.Constants.OIConstants;
 import org.sciborgs1155.robot.commands.Autos;
 import org.sciborgs1155.robot.commands.ExampleCommand;
 import org.sciborgs1155.robot.subsystems.ExampleSubsystem;
@@ -25,7 +25,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      new CommandXboxController(OIConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
