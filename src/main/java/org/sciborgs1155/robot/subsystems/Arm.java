50ca85b752e809447f9a287ab61997728122719e
Tabeeb <chowdhurt4@bxscience.edu>
2023-01-31 17:44:58 -0500

    fixed arm (made it subsystemBase)

--- src/main/java/org/sciborgs1155/robot/subsystems/Arm.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/Arm.java
@@ -5,12 +5,15 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import org.sciborgs1155.robot.Constants.Motors;
 import org.sciborgs1155.robot.Constants.PlacementConstants.Elbow;
 import org.sciborgs1155.robot.Ports.ArmPorts;
 
-public class Arm {
+public class Arm extends SubsystemBase{
   private final CANSparkMax armMotors =
       Motors.ARM.buildCanSparkMaxGearbox(MotorType.kBrushless, ArmPorts.armPorts);
   private final MotorControllerGroup armGroup = new MotorControllerGroup(armMotors);
@@ -19,17 +22,24 @@ public class Arm {
       new ArmFeedforward(Elbow.kS, Elbow.kG, Elbow.kV, Elbow.kA);
   private final ProfiledPIDController armFeedback =
       new ProfiledPIDController(Elbow.kP, Elbow.kI, Elbow.kD, Elbow.CONSTRAINTS);
-
+  private Rotation2d goal;
   public Arm() {
     armEncoder.setPositionConversionFactor(Elbow.GEAR_RATIO * Elbow.MOVEMENTPERSPIN);
     armEncoder.setVelocityConversionFactor(Elbow.GEAR_RATIO);
   }
-
-  public void setArmPosition(double position) {
-    double fb = armFeedback.calculate(armEncoder.getPosition(), position);
+  public void setGoal(Rotation2d goal) {
+      this.goal = goal;
+  }
+  @Override
+  public void periodic() {
+    double fb = armFeedback.calculate(
+      armEncoder.getPosition(), 
+      goal.getRadians());
     double ff =
         armFeedforward.calculate(
             armFeedback.getSetpoint().position, armFeedback.getSetpoint().velocity);
     armGroup.setVoltage(fb + ff);
+  
   }
+    
 }
