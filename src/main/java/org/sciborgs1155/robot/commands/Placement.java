61d52b85a62188e8df1fa80dda3b81206cb6bd2b
Andrew Herrera <andrewhts123x@gmail.com>
2023-02-10 16:55:53 -0500

    added goToState

--- src/main/java/org/sciborgs1155/robot/commands/Placement.java
+++ src/main/java/org/sciborgs1155/robot/commands/Placement.java
@@ -3,6 +3,8 @@ package org.sciborgs1155.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+
+import org.sciborgs1155.robot.Constants.Arm.Wrist;
 import org.sciborgs1155.robot.subsystems.Arm;
 import org.sciborgs1155.robot.subsystems.Elevator;
 
@@ -14,20 +16,42 @@ public final class Placement {
     public final Rotation2d elbowAngle;
     public final Rotation2d wristAngle;
 
+    public Arm.Side getSide() {
+      return elbowAngle.getCos() > 0 ? Arm.Side.FRONT : Arm.Side.BACK;
+    }
+
     public State(double elevatorHeight, Rotation2d elbowAngle, Rotation2d wristAngle) {
       this.elevatorHeight = elevatorHeight;
       this.elbowAngle = elbowAngle;
       this.wristAngle = wristAngle;
     }
-  }
 
+    
+  }
+  
   public static Command goToState(Arm arm, Elevator elevator, State state) {
+
+
+
+ 
+    double lowHeight =0 ;//placeholder garbage
+    
     return Commands.parallel(
-            elevator.setGoal(state.elevatorHeight),
+
+            /*
+            Drives elevator down to lowHeight if state side is opposite to arm side
+            
+
+
+            */
+            Commands.either(elevator.setGoal(lowHeight),Commands.none(),()->state.getSide()!=arm.getSide()).andThen(elevator.setGoal(state.elevatorHeight)),
+            
             arm.setElbowGoal(state.elbowAngle),
             arm.setAbsoluteWristGoal(state.wristAngle))
         .andThen(
-            Commands.waitUntil(() -> elevator.atGoal() && arm.atElbowGoal() && arm.atWrsitGoal()));
+            Commands.waitUntil(() -> elevator.atGoal() && arm.atElbowGoal() && arm.atWrsitGoal())
+            
+            );
   }
 
   public static Command goToState(Arm arm, Elevator elevator, State... states) {
