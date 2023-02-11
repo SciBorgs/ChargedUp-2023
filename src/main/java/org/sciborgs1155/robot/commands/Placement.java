a5a3ecb177f9e767a1e4c22938a2716ba7f0f380
Andrew Herrera <andrewhts123x@gmail.com>
2023-02-11 13:30:14 -0500

    commented placement

--- src/main/java/org/sciborgs1155/robot/commands/Placement.java
+++ src/main/java/org/sciborgs1155/robot/commands/Placement.java
@@ -1,33 +1,7 @@
 package org.sciborgs1155.robot.commands;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-
-import org.sciborgs1155.robot.Constants.Arm.Wrist;
-import org.sciborgs1155.robot.subsystems.Arm;
-import org.sciborgs1155.robot.subsystems.Elevator;
-
 /** Trajectory for elevator and arm, without respect for time */
 public final class Placement {
-
-  public static class State {
-    public final double elevatorHeight;
-    public final Rotation2d elbowAngle;
-    public final Rotation2d wristAngle;
-
-    public Arm.Side getSide() {
-      return elbowAngle.getCos() > 0 ? Arm.Side.FRONT : Arm.Side.BACK;
-    }
-
-    public State(double elevatorHeight, Rotation2d elbowAngle, Rotation2d wristAngle) {
-      this.elevatorHeight = elevatorHeight;
-      this.elbowAngle = elbowAngle;
-      this.wristAngle = wristAngle;
-    }
-
-    
-  }
 
   // public Command goToCameraTarget() {
   //   return Commands.either(
