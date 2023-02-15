753842e0c2a17a13ea5cb9c9e86fd481a5671ac4
dannynotsmart <75951607+dannynotsmart@users.noreply.github.com>
2023-02-14 21:26:20 -0500

    modified range values for testing

--- src/main/java/org/sciborgs1155/robot/subsystems/Intake.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/Intake.java
@@ -3,8 +3,6 @@ package org.sciborgs1155.robot.subsystems;
 import static org.sciborgs1155.robot.Constants.Intake.*;
 import static org.sciborgs1155.robot.Ports.Intake.*;
 
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
@@ -26,17 +24,16 @@ public class Intake extends SubsystemBase implements Loggable {
 
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 sensor = new ColorSensorV3(i2cPort);
-  private final ColorMatch matcher = new ColorMatch();
+  // private final ColorMatch matcher = new ColorMatch();
 
   public Intake() {
-    matcher.addColorMatch(GamePieces.NOTHING.color);
-    matcher.addColorMatch(GamePieces.CONE.color);
-    matcher.addColorMatch(GamePieces.CUBE.color);
+    // matcher.addColorMatch(GamePieces.CONE.color);
+    // matcher.addColorMatch(GamePieces.CUBE.color);
   }
 
   public GamePieces getGamePiece() {
     Color detected = sensor.getColor();
-    ColorMatchResult match = matcher.matchClosestColor(detected);
+    // ColorMatchResult match = matcher.matchClosestColor(detected);
 
     System.out.println(detected.red * 255 + " " + detected.green * 255 + " " + detected.blue * 255);
 
@@ -44,9 +41,17 @@ public class Intake extends SubsystemBase implements Loggable {
     double g = detected.green * 255;
     double b = detected.blue * 255;
 
-    if (r >= 75 && r <= 92 && g >= 127 && g <= 141 && b >= 20 && b <= 46) {
+    if (r >= 70 && r <= 95 && g >= 120 && g <= 145 && b >= 15 && b <= 50) {
+      /*
+       * old values were:
+       * r >= 75 && r <= 92 && g >= 127 && g <= 141 && b >= 20 && b <= 46
+       */
       return GamePieces.CONE;
-    } else if (r >= 55 && r <= 67 && g >= 87 && g <= 120 && b >= 69 && b <= 112) {
+    } else if (r >= 50 && r <= 70 && g >= 80 && g <= 125 && b >= 65 && b <= 120) {
+      /*
+       * old values were:
+       * r >= 55 && r <= 67 && g >= 87 && g <= 120 && b >= 69 && b <= 112
+       */
       return GamePieces.CUBE;
     } else {
       return GamePieces.NOTHING;
