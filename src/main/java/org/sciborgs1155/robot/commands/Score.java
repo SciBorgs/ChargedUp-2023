71fee38e060fbae803eb011eca8e122fefe1a209
sigalrmp <sigalrmp@gmail.com>
2023-03-08 21:18:48 -0500

    now it has scoring and aligning sort of but they're separate and there's still something that needs to be fixed and there isn't visionAlign yet

--- src/main/java/org/sciborgs1155/robot/commands/Score.java
+++ src/main/java/org/sciborgs1155/robot/commands/Score.java
@@ -11,12 +11,15 @@ import org.sciborgs1155.robot.subsystems.Arm;
 import org.sciborgs1155.robot.subsystems.Drive;
 import org.sciborgs1155.robot.subsystems.Elevator;
 import org.sciborgs1155.robot.subsystems.Intake;
+import org.sciborgs1155.robot.util.PlacementState;
 import org.sciborgs1155.robot.util.Vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class Score {
     private final Intake intake;
@@ -31,22 +34,30 @@ public class Score {
         this.vision = vision;
     }
 
+    public final Command score(GamePiece gamePiece, ScoringHeight height, Side side) {
+        if (height == ScoringHeight.HIGH && side == Side.BACK) {
+            throw new RuntimeException("cannot score high in the back");
+        }
+        return placement.toState(scoringState(gamePiece, height, side)).andThen(
+               intake.start(true)).andThen(Commands.waitSeconds(3)).andThen(intake.stop());
+    }
+
     // make it take gamePiece into account (maybe need to change format of field constants)
-    public final Command odometryAlign(RobotSide side, Color color) {
+    public final Command odometryAlign(Side side, Color color) {
         return drive.driveToPose(closestScoringPoint(side, color));
     }
 
-    private final Pose2d closestScoringPoint(RobotSide side, Color color) {
+    private final Pose2d closestScoringPoint(Side side, Color color) {
         Collection<Translation2d> scoringPoints = Constants.Field.SCORING_POINTS.values();
         Translation2d point = drive.getPose().getTranslation().nearest(new ArrayList<Translation2d>(List.copyOf(scoringPoints))); 
         // blue front: 180; blue back: 0; red front: 0; red front: 180
         double rotationDeg = 180;
-        if (side == RobotSide.BACK) { rotationDeg = (rotationDeg + 180) % 360; }
+        if (side == Side.BACK) { rotationDeg = (rotationDeg + 180) % 360; }
         if (color == Color.RED) { rotationDeg = (rotationDeg + 180) % 360; }
         return new Pose2d(point, Rotation2d.fromDegrees(rotationDeg));
     }
 
-    public enum RobotSide {
+    public enum Side {
         BACK,
         FRONT
     }
@@ -67,5 +78,55 @@ public class Score {
         LOW
     }
 
-    
+    public static PlacementState scoringState(
+            GamePiece gamePiece, ScoringHeight height, Side side) {
+        switch (gamePiece) {
+            case CONE:
+            switch (height) {
+                case HIGH:
+                switch (side) {
+                    case FRONT:
+                    return Constants.Positions.FRONT_HIGH_CONE;
+                    case BACK:
+                    return Constants.Positions.BACK_HIGH_CONE;
+                }
+                case MID:
+                switch (side) {
+                    case FRONT:
+                    return Constants.Positions.FRONT_MID_CONE;
+                    case BACK:
+                    return Constants.Positions.BACK_MID_CONE;
+                }
+                case LOW:
+                return Constants.Positions.BACK_LOW_CONE;
+            }
+            case CUBE:
+            switch (height) {
+                case HIGH:
+                switch (side) {
+                    case FRONT:
+                    return Constants.Positions.FRONT_HIGH_CUBE;
+                    case BACK:
+                    return Constants.Positions.BACK_HIGH_CUBE;
+                }
+                case MID:
+                switch (side) {
+                    case FRONT:
+                    return Constants.Positions.FRONT_MID_CUBE;
+                    case BACK:
+                    return Constants.Positions.BACK_MID_CUBE;
+                }
+                case LOW:
+                return Constants.Positions.BACK_LOW_CUBE;
+            }
+        }
+        throw new RuntimeException(
+            "scoringState was not called on a valid arguments. \n"
+                + "gamePiece: "
+                + gamePiece
+                + "; height: "
+                + height
+                + "; side: "
+                + side);
+        }
 }
