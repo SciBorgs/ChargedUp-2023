6aa91cf937d73518378461079b30a1b590ba6962
Tabeeb <chowdhurt4@bxscience.edu>
2023-03-11 12:03:10 -0500

    placementModel innit

---
+++ src/main/java/org/sciborgs1155/lib/PlacementModel.java
@@ -0,0 +1,25 @@
+package org.sciborgs1155.lib;
+
+
+public class PlacementModel{
+    public enum PlacementModels {
+        RestrictionLess(0),
+        ArmLock(1),
+        FloorBlock(2),
+        Restricted(3);
+
+        private int modelType;
+
+        private PlacementModels(int currentModel){
+            this.modelType = modelType;
+        }
+    }
+    PlacementModels currentModel = PlacementModels.Restricted;
+    public void setModel(PlacementModels desiredModel){
+        currentModel = desiredModel;
+    }
+    public PlacementModels getModel(){
+        return currentModel;
+    }
+      
+}
