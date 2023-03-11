88d3b24d090fc32f05093c7dadd894d66209d437
Tabeeb <chowdhurt4@bxscience.edu>
2023-03-11 12:43:39 -0500

    Spotless

--- src/main/java/org/sciborgs1155/lib/PlacementModel.java
+++ src/main/java/org/sciborgs1155/lib/PlacementModel.java
@@ -1,25 +1,26 @@
 package org.sciborgs1155.lib;
 
+public class PlacementModel {
+  public enum PlacementModels {
+    RestrictionLess(0),
+    ArmLock(1),
+    FloorBlock(2),
+    Restricted(3);
 
-public class PlacementModel{
-    public enum PlacementModels {
-        RestrictionLess(0),
-        ArmLock(1),
-        FloorBlock(2),
-        Restricted(3);
+    private int modelType;
 
-        private int modelType;
+    private PlacementModels(int currentModel) {
+      this.modelType = modelType;
+    }
+  }
 
-        private PlacementModels(int currentModel){
-            this.modelType = modelType;
-        }
-    }
-    PlacementModels currentModel = PlacementModels.Restricted;
-    public void setModel(PlacementModels desiredModel){
-        currentModel = desiredModel;
-    }
-    public PlacementModels getModel(){
-        return currentModel;
-    }
-      
+  PlacementModels currentModel = PlacementModels.Restricted;
+
+  public void setModel(PlacementModels desiredModel) {
+    currentModel = desiredModel;
+  }
+
+  public PlacementModels getModel() {
+    return currentModel;
+  }
 }
