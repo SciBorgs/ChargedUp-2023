a034bd04586fd105e4eae4857562e8ca0c761498
sigalrmp <sigalrmp@gmail.com>
2023-03-11 23:29:14 -0500

    i hate spotless

--- src/main/java/org/sciborgs1155/robot/RobotContainer.java
+++ src/main/java/org/sciborgs1155/robot/RobotContainer.java
@@ -1,7 +1,5 @@
 package org.sciborgs1155.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
