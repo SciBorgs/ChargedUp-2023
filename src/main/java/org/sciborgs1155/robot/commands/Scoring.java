72073386ab0f1ab5f718b2f8eea929977f882f09
Kishan <patelk3@bxscience.edu>
2023-03-13 08:40:09 -0400

    cone and cube LED almost done

--- src/main/java/org/sciborgs1155/robot/commands/Scoring.java
+++ src/main/java/org/sciborgs1155/robot/commands/Scoring.java
@@ -6,6 +6,8 @@ import com.pathplanner.lib.PathPoint;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.Command;
 import java.util.ArrayList;
 import org.sciborgs1155.robot.Constants;
@@ -17,6 +19,8 @@ public final class Scoring {
   private final Drive drive;
   public final Arm arm;
   public final Elevator elevator;
+  private AddressableLED led = new AddressableLED(0);
+  private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(0);
 
   public Scoring(Drive drive, Arm arm, Elevator elevator) {
     this.drive = drive;
@@ -33,5 +37,19 @@ public final class Scoring {
             new PathConstraints(Constants.Auto.MAX_SPEED, Constants.Auto.MAX_ACCEL), destination),
         false,
         false);
+  }
+
+  public void coneLED() {
+    for (int i = 0; i < ledBuffer.getLength(); i++) {
+      ledBuffer.setHSV(i, 57, 94, 98);
+    }
+    led.setData(ledBuffer);
+  }
+
+  public void cubeLED() {
+    for (int i = 0; i < ledBuffer.getLength(); i++) {
+      ledBuffer.setHSV(i, 236, 100, 100);
+    }
+    led.setData(ledBuffer);
   }
 }
