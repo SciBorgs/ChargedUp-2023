f30d599942bb1fc92481fb4696c57dc00adc2930
warren yun <wyun2006@gmail.com>
2023-03-14 02:52:50 -0400

    init solver client

--- src/main/java/org/sciborgs1155/robot/subsystems/placement/SolverClient.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/placement/SolverClient.java
@@ -1,3 +1,25 @@
 package org.sciborgs1155.robot.subsystems.placement;
 
-public class SolverClient {}
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StringPublisher;
+
+public class SolverClient {
+
+    private final StringPublisher requester;
+    private final DoubleArraySubscriber results;
+
+    public SolverClient() {
+        var chronosTable = NetworkTableInstance.getDefault().getTable("chronos");
+        requester = chronosTable.getStringTopic("request").publish(PubSubOption.periodic(0.0), PubSubOption.keepDuplicates(true));
+        results = chronosTable.getDoubleArrayTopic("result").subscribe(new double[] {}, PubSubOption.periodic(0.0));
+    }
+
+    public void request(State initialState, State finalState) {
+        
+    }
+
+
+
+}
