582b16cd53165957f14df7598c1ffcb8453ecc11
sigalrmp <sigalrmp@gmail.com>
2023-04-30 19:43:24 -0400

    added some testing files that should be written

---
+++ src/test/java/org/sciborgs1155/robot/commands/PlacementTest.java
@@ -0,0 +1,5 @@
+package org.sciborgs1155.robot.commands;
+
+public class PlacementTest {
+    
+}
---
+++ src/test/java/org/sciborgs1155/robot/util/placement/PlacementStateTest.java
@@ -0,0 +1,5 @@
+package org.sciborgs1155.robot.util.placement;
+
+public class PlacementStateTest {
+    
+}
