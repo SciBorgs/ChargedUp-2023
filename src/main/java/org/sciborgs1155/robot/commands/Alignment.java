75264e955e0066628449987f63d1d828f029f172
Asa Paparo <asapaparo@gmail.com>
2023-06-07 12:55:31 -0400

    added back alignment

--- src/main/java/org/sciborgs1155/robot/commands/Alignment.java
+++ src/main/java/org/sciborgs1155/robot/commands/Alignment.java
@@ -1,43 +1,36 @@
-// package org.sciborgs1155.robot.commands;
+package org.sciborgs1155.robot.commands;
 
-// import static org.sciborgs1155.robot.Constants.Field.*;
+import static org.sciborgs1155.robot.Constants.Field.*;
 
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.geometry.Translation2d;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import java.util.Collection;
-// import java.util.List;
-// import org.sciborgs1155.robot.subsystems.Drive;
-// import org.sciborgs1155.robot.subsystems.arm.ArmState.GamePiece;
-// import org.sciborgs1155.robot.subsystems.arm.ArmState.Side;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import java.util.Collection;
+import java.util.List;
+import org.sciborgs1155.robot.subsystems.Drive;
+import org.sciborgs1155.robot.subsystems.arm.ArmState.GamePiece;
+import org.sciborgs1155.robot.subsystems.arm.ArmState.Side;
 
-// public class Alignment {
+public class Alignment {
 
-//   private final Drive drive;
+  private final Drive drive;
 
-//   public Alignment(Drive drive) {
-//     this.drive = drive;
-//   }
+  public Alignment(Drive drive) {
+    this.drive = drive;
+  }
 
-//   public Command odometryAlign() {
-//     return odometryAlign(drive.getPose());
-//   }
+  public Command odometryAlign(Pose2d startPose, GamePiece gamePiece, Side side) {
+    return drive.driveToPose(startPose, closestScoringPoint(startPose, gamePiece, side), true);
+  }
 
-//   public Command odometryAlign(Pose2d startPose, GamePiece gamePiece) {
-//     return drive.driveToPose(startPose, closestScoringPoint(startPose, side), true);
-//   }
-
-//   private Pose2d closestScoringPoint(Pose2d pose, Side side, GamePiece gamePiece) {
-//     Collection<Translation2d> scoringPoints =
-//         switch (gamePiece) {
-//           case CONE -> SCORING_POINTS_CONE.values();
-//           case CUBE -> SCORING_POINTS_CUBE.values();
-//         };
-//     Translation2d point = pose.getTranslation().nearest(List.copyOf(scoringPoints));
-//     return new Pose2d(point, Rotation2d.fromRadians(side.angle));
-//   }
-// }
-
-// TODO temporarily commented out
-// replace scoring points with pose2d
+  private Pose2d closestScoringPoint(Pose2d pose, GamePiece gamePiece, Side side) {
+    Collection<Translation2d> scoringPoints =
+        switch (gamePiece) {
+          case CONE -> SCORING_POINTS_CONE.values();
+          case CUBE -> SCORING_POINTS_CUBE.values();
+        };
+    Translation2d point = pose.getTranslation().nearest(List.copyOf(scoringPoints));
+    return new Pose2d(point, Rotation2d.fromRadians(side.angle));
+  }
+}
