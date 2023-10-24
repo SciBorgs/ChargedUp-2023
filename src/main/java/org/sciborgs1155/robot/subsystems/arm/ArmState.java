66312dad446406ad65a8aab143c341aeb215a82f
Yxhej <wui@bxscience.edu>
2023-10-24 16:29:35 -0400

    spotless

--- src/main/java/org/sciborgs1155/robot/subsystems/arm/ArmState.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/arm/ArmState.java
@@ -5,7 +5,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.DoubleUnaryOperator;
